f9cf28788dd06c1d618a9d5c6b8506d195c38835
sandy <sandy@Sandy>
2017-04-16 14:03:32 -0500

    added one more sop by sandeep for testing

--- src/com/practice/threads/TestThread.java
+++ src/com/practice/threads/TestThread.java
@@ -6,6 +6,7 @@ public class TestThread {
 		RunnableDemo R1 = new RunnableDemo("Thread-1");
 		R1.start();
 		System.out.println("this is test");
+		System.out.println("second test");
 		RunnableDemo R2 = new RunnableDemo("Thread-2");
 		R2.start();
 	}
