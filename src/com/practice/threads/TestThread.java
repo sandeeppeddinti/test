51b01e8a80418a299846a57e31244c3b3c6f0ad3
nookaraju <rajuchit04@gmail.com>
2017-04-16 13:34:09 -0500

    added sop statement

--- src/com/practice/threads/TestThread.java
+++ src/com/practice/threads/TestThread.java
@@ -5,7 +5,7 @@ public class TestThread {
 	public static void main(String args[]) {
 		RunnableDemo R1 = new RunnableDemo("Thread-1");
 		R1.start();
-
+		System.out.println("this is test");
 		RunnableDemo R2 = new RunnableDemo("Thread-2");
 		R2.start();
 	}
